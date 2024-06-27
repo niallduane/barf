b72ed3e369907729db6f98fedaa341e3225ee0f4
Niall Duane <niall.duane@craigsip.com>
2024-06-27 20:54:58 +1200

    10 added documentation

--- lib/installLocally.sh
+++ lib/installLocally.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-dotnet pack
+version=1.0.0
+
+dotnet pack /p:PackageVersion="$version"
 dotnet tool uninstall Tada --global
 dotnet tool install Tada --global
 
 dotnet new uninstall Tada.TemplatePack
-dotnet new install ./output/Tada.TemplatePack.1.0.0.nupkg --force
+dotnet new install "./output/Tada.TemplatePack.$version.nupkg" --force
