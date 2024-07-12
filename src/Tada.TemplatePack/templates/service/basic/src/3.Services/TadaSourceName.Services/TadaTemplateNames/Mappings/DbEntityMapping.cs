9055cea1e4502fa6c09c4485113f6db6a47fbdc3
Niall Duane <niall.duane@craigsip.com>
2024-07-12 22:08:23 +1200

    Update DbEntityMapping

--- src/Tada.TemplatePack/templates/service/basic/src/3.Services/TadaSourceName.Services/TadaTemplateNames/Mappings/DbEntityMapping.cs
+++ src/Tada.TemplatePack/templates/service/basic/src/3.Services/TadaSourceName.Services/TadaTemplateNames/Mappings/DbEntityMapping.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TadaSourceName.Domain.Core;
+using TadaSourceName.Domain.Core.Extensions;
 using TadaSourceName.Domain.Services.TadaTemplateNames.Models;
 
 namespace TadaSourceName.Services.TadaTemplateNames.Mappings;
@@ -28,6 +29,22 @@ internal static class DbEntityMapping
         {
 
         };
+    }
+
+    public static Dictionary<string, object?> ToEntityProperties(this UpsertTadaTemplateNameRequest obj)
+    {
+        var props = Json
+            .ToDictionary(obj)
+            .MapToObjectProperties<TadaTemplateName>();
+            
+        // add/remove custom mappings here
+        return props;
+    }
+    public static Dictionary<string, object?> ToEntityProperties(this PatchRequest<UpdateTadaTemplateNameRequest> obj)
+    {
+        var props = obj.MapToObjectProperties<TadaTemplateName>();
+        // add/remove custom mappings here
+        return props;
     }
 
     public static UpdateTadaTemplateNameResponse ToUpdateTadaTemplateNameResponse(this object obj)
